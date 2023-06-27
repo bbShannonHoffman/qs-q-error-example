68f0c3b677e94b02b2a5c6b231254802478de64f
bbShannonHoffman <shannon.hoffman@blackboard.com>
2023-06-27 09:49:23 -0400

    init.

---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <h2>Click on QuickSight Q, then return to this page to see the error.</h2>;
+}
+
+export default Home;
---
+++ src/components/QSearchBar.tsx
@@ -0,0 +1,68 @@
+import { createEmbeddingContext } from 'amazon-quicksight-embedding-sdk';
+import {
+    QSearchFrame,
+    QSearchContentOptions,
+    FrameOptions,
+} from 'amazon-quicksight-embedding-sdk/dist/types';
+import React from 'react';
+
+export const QSearchBar: React.FunctionComponent = () => {
+
+    const qSearchBarRef = React.useRef<QSearchFrame>();
+
+    const embed = React.useCallback(async () => {
+        const embeddingContext = await createEmbeddingContext();
+        const { embedQSearchBar } = embeddingContext;
+
+        const frameOptions: FrameOptions = {
+            url: 'YOUR_Q_SEARCH_BAR_URL',
+            height: 'AutoFit',
+            container: '#q-searchbar-container',
+            onChange: changeEvent => {
+                if (changeEvent.eventLevel === 'ERROR') {
+                    console.error(
+                        'QuickSight Q threw the following error: ',
+                        changeEvent
+                    );
+                }
+            },
+        };
+
+        const contentOptions: QSearchContentOptions = {
+            hideIcon: true,
+            hideTopicName: false,
+            theme: 'CLASSIC', // TODO: Add a real themeId here
+            allowTopicSelection: true,
+            onMessage: messageEvent => {
+                switch (messageEvent.eventName) {
+                    case 'CONTENT_LOADED': {
+                        console.log('Q Search Bar loaded');
+                        break;
+                    }
+                    case 'ERROR_OCCURRED': {
+                        console.error('Q Search Bar error: ', messageEvent);
+                        break;
+                    }
+                }
+            },
+        };
+
+        await embedQSearchBar(frameOptions, contentOptions);
+    }, []);
+
+    React.useEffect(() => {
+        if (!qSearchBarRef.current) {
+            embed().catch(() => {
+                console.error('Failed to embed Q Search Bar');
+            });
+        }
+    }, [embed, qSearchBarRef]);
+
+    return (
+        <div
+            data-testid="q-searchbar-container"
+            id="q-searchbar-container"
+            title="Q Search Bar"
+        />
+    );
+};
